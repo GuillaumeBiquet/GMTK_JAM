c52b5e2cfb7f147c7d1e67e365aad40dd9ec0b75
Otolepp <guillaumebiquet94@gmail.com>
2021-06-12 13:26:12 +0200

    update
    Spawner Fixe au milieux
    Liaison entre ship (contrainte de taille)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+
+    [SerializeField] int maxShips = 100;
+    [System.NonSerialized] public int nbShips = 0;
+
+
+    public bool TooManyShips { get { return nbShips >= maxShips; } }
+
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject shipPrefab;
+    [SerializeField] float spawnRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        while (gameObject.scene.IsValid())
+        {
+            if (GameManager.Instance.TooManyShips)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+            else
+            {
+                Instantiate(shipPrefab, transform.position, Quaternion.identity);
+                GameManager.Instance.nbShips++;
+                yield return new WaitForSeconds(spawnRate);
+            }
+        }
+    }
+
+}
