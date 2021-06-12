395f71c229bc8a3d063164ae44e1be9bf523222a
F. Kuhlmann <36305934+Thiiamas@users.noreply.github.com>
2021-06-12 13:38:04 +0200

    Update prefab + particles

--- Assets/Scripts/Player/Bullet1.cs
+++ Assets/Scripts/Player/Bullet1.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Bullet1 : Bullet
 {
+    [SerializeField] ParticleSystem explo;
 
     public override void Launch(Vector2 playerVelocity ,Vector2 direction)
     {
         rb.velocity = playerVelocity;
         rb.AddForce(direction.normalized * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Wall")
+        {
+            Debug.Log("Collided");
+            Instantiate(explo, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
