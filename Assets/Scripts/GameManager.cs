1ff59579145f2f73797d3847e9454f79af8e877a
F. Kuhlmann <36305934+Thiiamas@users.noreply.github.com>
2021-06-12 18:42:16 +0200

    Create Menu, add Timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,7 +14,7 @@ public class GameManager : MonoBehaviour
 
     //Scoring
     [SerializeField] TextMeshProUGUI TMScore;
-    [SerializeField] public int score;
+    [SerializeField] public float score;
     float timer = 0f;
 
     public bool TooManyShips { get { return nbShips >= maxShips; } }
@@ -34,13 +34,16 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        timer += Time.deltaTime;
-        if (timer > 1)
-        {
-            score += 10;
-            timer = 0;
-        }
-        TMScore.text = score.ToString();
+        score += Time.deltaTime;
+        /* timer += Time.deltaTime;
+         if (timer > 1)
+         {
+             score += 10;
+             timer = 0;
+         }*/
+        float min = Mathf.Round(score / 60);
+        float sec = score % 60;
+        TMScore.text = min + ":" + sec.ToString("F0");
     }
 
 
